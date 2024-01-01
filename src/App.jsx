3a9991877e216c59b5a8279e4830a9fe100c8c9a
GreyyDaze <117322834+GreyyDaze@users.noreply.github.com>
2024-01-01 05:13:43 +0500

    final template

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import HeroSection from "./components/HeroSection";
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Routes>
+          <Route path={"/"} element={<HeroSection />}></Route>
+        </Routes>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/const/APIs.js
@@ -0,0 +1,11 @@
+export const BASE_URL_EXAM = "https://fakestoreapi.com";
+export const APIs = {
+  ECOMMERCE: {
+    PRODUCTS: {
+      GET_ALL_PRODUCT: `${BASE_URL_SERVER}/products`,
+      ADD_PRODUCT: `${BASE_URL_SERVER}/products`,
+      GET_SINGLE_PRODUCT: `${BASE_URL_SERVER}/products`,
+    },
+    CART: {},
+  },
+};
